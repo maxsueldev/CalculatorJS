b4aac040ccad47be0d2bb88db6dcf4c8f678aad2
Maxsuel Fernando <maxsuel.dev@gmail.com>
2022-12-31 09:31:03 -0300

    toogleOnOff function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let isOn = false;
+
 const screen = document.querySelector(".screen");
+const screenText = screen.querySelector("h1");
+const onOff = document.querySelector("button.on-off");
+
+function toogleOnOff() {
+    if(isOn) {
+        isOn = false;
+        screenText.textContent = "";
+    } else {
+        isOn = true;
+        screenText.textContent = "0";
+    }
+}
+
+onOff.addEventListener("click", toogleOnOff);
