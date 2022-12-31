5baebe46b51dcef0d88b599192709e3bd3a0ecad
Maxsuel Fernando <maxsuel.dev@gmail.com>
2022-12-31 11:37:42 -0300

    CE calculator button & function; checkIsOn function

--- script.js
+++ script.js
@@ -2,16 +2,42 @@ let isOn = false;
 
 const screen = document.querySelector(".screen");
 const screenText = screen.querySelector("h1");
+
+// const buttons = document.querySelectorAll("button");
+// const onOff = buttons.querySelector(".on-off");
+
 const onOff = document.querySelector("button.on-off");
+const ce = document.querySelector("button.ce");
 
 function toogleOnOff() {
     if(isOn) {
         isOn = false;
         screenText.textContent = "";
+        // console.log(isOn);
     } else {
         isOn = true;
         screenText.textContent = 0;
+        // console.log(isOn);
+    }
+    return isOn;
+}
+
+function checkIsOn() {
+    if(!isOn) {
+        return;
+    }
+    return true;
+}
+
+function ceFunction() {
+    if(checkIsOn()) {
+        screenText.textContent = "test";
     }
 }
 
+
+
+// console.log(isOn);
+
 onOff.addEventListener("click", toogleOnOff);
+ce.addEventListener("click", ceFunction);
