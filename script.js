4d0fd8e79acdf08dc104d8fecd7cc5c91dca7125
Maxsuel Fernando <maxsuel.dev@gmail.com>
2023-01-20 13:42:15 -0300

    created buttons and off function

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ const screenText = screen.querySelector("h1");
 
 const onC = document.querySelector("button.onC");
 const ce = document.querySelector("button.ce");
+const btnOff = document.querySelector("button.btnOff");
+console.log(btnOff);
 
+// console.log(buttonsCalc);
+
+// Pegar todos os botoes da calculadora
+const buttonsCalc = document.querySelectorAll("button");
+
+const btns = [...buttonsCalc];
+console.log(btns);
+
+
+    
 function toogleOnClear() {  // On ou Clear
   // Se estiver desligada, liga. Se estiver ligada, zera.
     if(!isOn) {
@@ -13,7 +25,7 @@ function toogleOnClear() {  // On ou Clear
     } 
     screenText.textContent = 0;
     return isOn;
-}
+} 
 
 function checkIsOn() {
     if(!isOn) {
@@ -22,9 +34,30 @@ function checkIsOn() {
     return true;
 }
 
+function offFunction() {
+    isOn = false;
+    screenText.textContent = "";
+}
+
 function ceFunction() {
     
 }
 
+function registerEvents(event) {
+    buttonsCalc.forEach(element => {
+        
+    });
+}
+
+
+
+// Fazer um map para percorrer os botoes de numeros
+btnOff.addEventListener("click", offFunction);
 onC.addEventListener("click", toogleOnClear);
 ce.addEventListener("click", ceFunction);
+
+btns.addEventListener("click", function(event){
+    console.log(event);
+});
+
+window.addEventListener("load", registerEvents);
